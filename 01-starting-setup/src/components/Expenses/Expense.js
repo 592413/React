f46e6729c082cb35a923bb47eeb27b215880cadc
Bappaditya Dutta <bappadityadutta2019@gmail.com>
2022-05-25 23:09:17 +0530

    Addinf Filter

--- 01-starting-setup/src/components/Expenses/Expense.js
+++ 01-starting-setup/src/components/Expenses/Expense.js
@@ -1,15 +1,27 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import React , {useState} from "react";
 import './Expense.css';
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expense(props){
+
+  const [filteredYear,setFilteredYear] =useState('2020');
+
+  const filterChangeHandler = (filterYear) =>{
+    setFilteredYear(filterYear);
+
+  }
+
+  const filteredExpenses= props.data.filter(expense => {
+    return expense.date.getFullYear().toString()===filteredYear;
+  })
+
+  
     return (
     <Card className='expenses'>
-      {props.data.map((expense) => <ExpenseItem
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}/>
-      )}
+      <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}/>
+      <ExpensesList items={filteredExpenses} />
     </Card >
     );
 }
