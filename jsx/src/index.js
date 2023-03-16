a5556326bef3ebfe2716ba51cb31c48ba7131694
592413 <bappadityadutta2019@gmail.com>
2023-03-16 09:59:53 +0530

    Creation of separate component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,7 @@
 //1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the Div with ID root
 
@@ -10,12 +11,7 @@ const el=document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-//4) Create a component
 
-function App(){
-    return <h1>Hi There </h1>
-}
-
-//5) Show the component on the screen
+//4) Show the component on the screen
 
 root.render(<App/>);
